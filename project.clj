edd2d039a9824165a1cdc9e9cd5fcf4b1d1e8ba2
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2011-02-28 16:19:17 +0900

    clojure 1.3alpha4

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject visugit "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :warn-on-reflection true,
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.automata/rosado.processing "1.1.0"]
+                 [org.clojars.hozumi/toxiclibscore "0020"]
+                 [org.clojars.hozumi/verletphysics "0020"]
                  [org.apache.commons/commons-exec "1.1"]
                  [org.clojars.hozumi/clj-glob "0.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
